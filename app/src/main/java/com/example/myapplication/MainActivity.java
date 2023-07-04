46a999f7962b1af08ba93d5870fdd9656c404518
KMS <80440158+ddolly518@users.noreply.github.com>
2023-07-05 02:58:37 +0900

    로그인&회원가입&로그아웃 완성 하단 네비게이션 바 추가

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth;
+
+    HomeFragment homeFragment;
+    DiaryFragment diaryFragment;
+    BulletinFragment bulletinFragment;
+    SettingFragment settingFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +37,36 @@ public class MainActivity extends AppCompatActivity {
         });
         //탈퇴처리
         //mFirebaseAuth.getCurrentUser().delete();
+
+        homeFragment = new HomeFragment();
+        diaryFragment = new DiaryFragment();
+        bulletinFragment = new BulletinFragment();
+        settingFragment = new SettingFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.containers, homeFragment).commit();
+
+        NavigationBarView navigationBarView = findViewById(R.id.bottom_navigationview);
+        navigationBarView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, homeFragment).commit();
+                        return true;
+                    case R.id.diary:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, diaryFragment).commit();
+                        return true;
+                    case R.id.bulletin:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, bulletinFragment).commit();
+                        return true;
+                    case R.id.setting:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, settingFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+
     }
 }
