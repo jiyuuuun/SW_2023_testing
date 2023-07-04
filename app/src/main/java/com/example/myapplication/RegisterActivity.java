045d231fe9fbf33713d565d381ec61dd770e1dcb
KMS <80440158+ddolly518@users.noreply.github.com>
2023-07-04 21:23:32 +0900

    logout

--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -1,17 +1,12 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -35,30 +30,24 @@ public class RegisterActivity extends AppCompatActivity {
         et_pass = findViewById(R.id.et_pass);
         btn_register = findViewById(R.id.btn_register);
 
-        btn_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String strEmail = et_email.getText().toString();
-                String strPass = et_pass.getText().toString();
+        btn_register.setOnClickListener(view -> {
+            String strEmail = et_email.getText().toString();
+            String strPass = et_pass.getText().toString();
 
-                mFirebaseAuth.createUserWithEmailAndPassword(strEmail, strPass).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
-                            UserAccount account = new UserAccount();
-                            account.setIdToken(firebaseUser.getUid());
-                            account.setEmailId(firebaseUser.getEmail());
-                            account.setPassword(strPass);
+            mFirebaseAuth.createUserWithEmailAndPassword(strEmail, strPass).addOnCompleteListener(RegisterActivity.this, task -> {
+                if (task.isSuccessful()) {
+                    FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+                    UserAccount account = new UserAccount();
+                    account.setIdToken(firebaseUser.getUid());
+                    account.setEmail(firebaseUser.getEmail());
+                    account.setPassword(strPass);
 
-                            mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
-                            Toast.makeText(RegisterActivity.this, "회원가입에 성공했습니다", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(RegisterActivity.this, "회원가입에 실패했습니다", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-            }
+                    mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
+                    Toast.makeText(RegisterActivity.this, "회원가입에 성공했습니다", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(RegisterActivity.this, "회원가입에 실패했습니다", Toast.LENGTH_SHORT).show();
+                }
+            });
         });
     }
 }
--- app/src/main/java/com/example/myapplication/UserAccount.java
+++ app/src/main/java/com/example/myapplication/UserAccount.java
@@ -3,14 +3,14 @@ package com.example.myapplication;
 public class UserAccount {
     public UserAccount() {}
     private String idToken;
-    private String emailId;
+    private String email;
     private String password;
 
     public void setIdToken(String idToken) {this.idToken = idToken;}
     public String getIdToken() {return idToken;}
 
-    public void setEmailId(String emailId) {this.emailId = emailId;}
-    public String getEmailId() {return emailId;}
+    public void setEmail(String emailId) {this.email = email;}
+    public String getEmail() {return email;}
 
     public void setPassword(String password) {this.password = password;}
     public String getPassword() {return password;}
