b1ea2b0512fd9160cd03de5d77072c9b645dfac3
KMS <80440158+ddolly518@users.noreply.github.com>
2023-07-05 02:58:37 +0900

    로그인&회원가입&로그아웃 완성

--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -43,6 +44,8 @@ public class RegisterActivity extends AppCompatActivity {
 
                     mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
                     Toast.makeText(RegisterActivity.this, "회원가입에 성공했습니다", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(intent);
                 } else {
                     Toast.makeText(RegisterActivity.this, "회원가입에 실패했습니다", Toast.LENGTH_SHORT).show();
                 }
