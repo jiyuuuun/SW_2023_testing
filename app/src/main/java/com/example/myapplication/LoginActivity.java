e6e524f17215949380df9be6910e9eb0520a08fa
KMS <80440158+ddolly518@users.noreply.github.com>
2023-07-10 19:04:36 +0900

    db 변경 완료

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,16 +2,17 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginActivity extends AppCompatActivity {
-    private FirebaseAuth mFirebaseAuth;
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private EditText et_email, et_pass;
 
     @Override
@@ -19,30 +20,28 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mFirebaseAuth = FirebaseAuth.getInstance();
-
         et_email = findViewById(R.id.et_email);
         et_pass = findViewById(R.id.et_pass);
-        Button btn_login = findViewById(R.id.btn_login);
-        Button btn_register = findViewById(R.id.btn_register);
-
-        btn_login.setOnClickListener(view -> {
-            String strEmail = et_email.getText().toString();
-            String strPass = et_pass.getText().toString();
-
-            mFirebaseAuth.signInWithEmailAndPassword(strEmail, strPass).addOnCompleteListener(LoginActivity.this, task -> {
-                if (task.isSuccessful()) {
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
-                }
-            });
-        });
-        btn_register.setOnClickListener(view -> {
-            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-            startActivity(intent);
-        });
+        findViewById(R.id.btn_login).setOnClickListener(this);
+        findViewById(R.id.btn_register).setOnClickListener(this);
+    }
+    @SuppressLint("NonConstantResourceId")
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_login:
+                mAuth.signInWithEmailAndPassword(et_email.getText().toString(), et_pass.getText().toString())
+                        .addOnCompleteListener(this, task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            } else {
+                                Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                break;
+            case R.id.btn_register:
+                startActivity(new Intent(this, RegisterActivity.class));
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -4,24 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class RegisterActivity extends AppCompatActivity {
-    private FirebaseAuth mFirebaseAuth;
-    private FirebaseFirestore mStore;
-
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private final FirebaseFirestore mStore = FirebaseFirestore.getInstance();
     private EditText et_email, et_pass;
 
     @Override
@@ -29,35 +26,28 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        mStore = FirebaseFirestore.getInstance();
-
         et_email = findViewById(R.id.et_email);
         et_pass = findViewById(R.id.et_pass);
-        Button btn_register = findViewById(R.id.btn_register);
-
-        btn_register.setOnClickListener(view -> {
-            String strEmail = String.valueOf(et_email.getText());
-            String strPass = String.valueOf(et_pass.getText());
-
-            mFirebaseAuth.createUserWithEmailAndPassword(strEmail, strPass).addOnCompleteListener(RegisterActivity.this, task -> {
-                if (task.isSuccessful()) {
-                    FirebaseUser user = mFirebaseAuth.getCurrentUser();
-                    if (user != null) {
-                        Map<String, Object> userMap = new HashMap<>();
-                        userMap.put(UserAccount.documentId, user.getUid());
-                        userMap.put(UserAccount.email, strEmail);
-                        userMap.put(UserAccount.password, strPass);
-                        mStore.collection(UserAccount.user).document(user.getUid()).set(userMap, SetOptions.merge());
-                        finish();
+        findViewById(R.id.btn_register).setOnClickListener(this);
+    }
+    public void onClick(View v) {
+        mAuth.createUserWithEmailAndPassword(et_email.getText().toString(), et_pass.getText().toString())
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        if (user != null) {
+                            Map<String, Object> userMap = new HashMap<>();
+                            userMap.put(UserAccount.documentId, user.getUid());
+                            userMap.put(UserAccount.email, et_email.getText().toString());
+                            userMap.put(UserAccount.password, et_pass.getText().toString());
+                            mStore.collection(UserAccount.user).document(user.getUid()).set(userMap, SetOptions.merge());
+                            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                            Toast.makeText(RegisterActivity.this, "회원 등록에 성공했습니다", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "회원 등록에 실패했습니다", Toast.LENGTH_SHORT).show();
                     }
-                    Toast.makeText(RegisterActivity.this, "회원가입에 성공했습니다", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(RegisterActivity.this, "회원가입에 실패했습니다", Toast.LENGTH_SHORT).show();
-                }
-            });
-        });
+                });
     }
 }
